f396f90ab465c564ab63cb806b171b15b39def72
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 02:37:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 tg.MainButton.show();
 
+
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
       tg.sendData("some string that we need to send"); 
       //при клике на основную кнопку отправляем данные в строковом виде
