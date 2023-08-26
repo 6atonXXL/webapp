809924b5cd75b7db9c2f4554f9709f72a1a8421f
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 02:10:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,15 +9,6 @@ tg.MainButton.show();
 let ipAddress = ""; // Переменная для хранения IP-адреса
 
 
-Telegram.WebApp.onEvent("mainButtonClicked", function getIPAddress() {
-    fetch("https://api.ipify.org?format=json")
-        .then(response => response.json())
-        .then(data => {
-            ipAddress = data.ip; // Сохраняем IP-адрес в переменной
-            document.getElementById("ipAddress").textContent = "Your IP Address: " + ipAddress;
-        })
-        .catch(error => {
-            console.error("Error fetching IP address:", error);
-        });
-        tg.sendData(ipAddress);
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    tg.sendData("Пуки каки");
 })
