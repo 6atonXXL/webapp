aec29d284077fda622e450c489833e9e7da3efae
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 02:34:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,7 @@ tg.MainButton.color = "#FF00FF";
 let ipAddress = ""; // Переменная для хранения IP-адреса
 
 
-btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-	if (tg.MainButton.isVisible){ //если кнопка показана 
-		tg.MainButton.hide() //скрываем кнопку 
-	}
-  else{ //иначе
-  	tg.MainButton.show() //показываем 
-  }
-});
-
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData("some string that we need to send"); 
-	//при клике на основную кнопку отправляем данные в строковом виде
-});
+      tg.sendData("some string that we need to send"); 
+      //при клике на основную кнопку отправляем данные в строковом виде
+   });
