c7a7a9a9beeaedf9c7190fd39afdb763aca79ec0
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 01:23:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ tg.expand(); //расширяем на все окно
 
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
+tg.MainButton.show();
 
-let btn = document.getElementById('btn');
+let ipAddress = ""; // Переменная для хранения IP-адреса
 
-btn.addEventListener('click', function () {
-    tg.MainButton.setText('Сообщение отправлено');
-    tg.MainButton.show();
-    tg.sendData('sendTestMessage');
+        function getIPAddress() {
+            fetch("https://api.ipify.org?format=json")
+                .then(response => response.json())
+                .then(data => {
+                    ipAddress = data.ip; // Сохраняем IP-адрес в переменной
+                    document.getElementById("ipAddress").textContent = "Your IP Address: " + ipAddress;
+                })
+                .catch(error => {
+                    console.error("Error fetching IP address:", error);
+                });
+        }
 
+Telegram.WebApp.onEvent("mainButtonClicked", function getIPAddress() {
+    fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            ipAddress = data.ip; // Сохраняем IP-адрес в переменной
+            document.getElementById("ipAddress").textContent = "Your IP Address: " + ipAddress;
+        })
+        .catch(error => {
+            console.error("Error fetching IP address:", error);
+        });
+        tg.sendData(ipAddress);
 })
