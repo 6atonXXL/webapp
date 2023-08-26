23110237d03c9124de92308b1021922ef36ffec8
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 01:28:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,29 +6,6 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 tg.MainButton.show();
 
-let ipAddress = ""; // Переменная для хранения IP-адреса
-
-        function getIPAddress() {
-            fetch("https://api.ipify.org?format=json")
-                .then(response => response.json())
-                .then(data => {
-                    ipAddress = data.ip; // Сохраняем IP-адрес в переменной
-                    document.getElementById("ipAddress").textContent = "Your IP Address: " + ipAddress;
-                })
-                .catch(error => {
-                    console.error("Error fetching IP address:", error);
-                });
-        }
-
-Telegram.WebApp.onEvent("mainButtonClicked", function getIPAddress() {
-    fetch("https://api.ipify.org?format=json")
-        .then(response => response.json())
-        .then(data => {
-            ipAddress = data.ip; // Сохраняем IP-адрес в переменной
-            document.getElementById("ipAddress").textContent = "Your IP Address: " + ipAddress;
-        })
-        .catch(error => {
-            console.error("Error fetching IP address:", error);
-        });
-        tg.sendData(ipAddress);
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData("12345");
 })
