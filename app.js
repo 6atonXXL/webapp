4cad634ab309e8b01312526363afa1d58990d106
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 01:40:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#FF00FF";
 tg.MainButton.show();
 
-Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData("12345");
-})
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+	tg.sendData("some string that we need to send"); 
+	//при клике на основную кнопку отправляем данные в строковом виде
+});
