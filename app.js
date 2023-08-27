86e40b040ae27d8869314f9d7142f183ca996dc2
Evgeny <90213272+6atonXXL@users.noreply.github.com>
2023-08-27 03:05:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 let btn = document.getElementById("btn");
 tg.expand(); //расширяем на все окно  
 
-btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-      if (tg.MainButton.isVisible){ //если кнопка показана 
-         tg.MainButton.hide() //скрываем кнопку 
-      }
-      else{ //иначе
-         tg.MainButton.show() //показываем 
-      }
-   });
+btn.addEventListener('click', function () { //вешаем событие на нажатие html-кнопки
+    if (tg.MainButton.isVisible) { //если кнопка показана 
+        tg.MainButton.hide() //скрываем кнопку 
+    }
+    else { //иначе
+        tg.MainButton.show() //показываем 
+    }
+});
 
 
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-      
-    $.getJSON('https://json.geoiplookup.io/api?callback=?', function(data) {
-    tg.sendData(data.ip); 
-})});
+Telegram.WebApp.onEvent('mainButtonClicked', function () {
+    tg.sendData("Hellow kakish");
+});
